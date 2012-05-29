c9e3d0cbdc54a3fa73044b2b6be227b3f1bab264
Steven Jones <steven.jones@computerminds.co.uk>
2012-05-29 19:51:33 +0100

    Use the right update command.

--- puppet/modules/aegir_ci_updater/manifests/init.pp
+++ puppet/modules/aegir_ci_updater/manifests/init.pp
@@ -4,7 +4,7 @@
 class aegir_ci_updater {
 
   cron { aegir_ci_updater:
-    command => "python /opt/aegir_ci/update.py",
+    command => "python /opt/aegir-ci/update.py",
     user => root,
     hour => 2,
     minute => 0,
