7eb730ea6385b1ea374bf3c6335b0a8b230c6561
Steven Jones <steven.jones@computerminds.co.uk>
2012-05-29 19:51:01 +0100

    Rename the updater

---
+++ puppet/modules/aegir_ci_updater/manifests/init.pp
@@ -0,0 +1,14 @@
+# Class: aegir_ci_updater
+#
+#   This module allows aegir_ci to auto-update.
+class aegir_ci_updater {
+
+  cron { aegir_ci_updater:
+    command => "python /opt/aegir_ci/update.py",
+    user => root,
+    hour => 2,
+    minute => 0,
+    ensure => 'present',
+  }
+
+}
