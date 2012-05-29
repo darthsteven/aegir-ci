cacdf7c0c35eccf09fea051280a825bd3501f577
Steven Jones <steven.jones@computerminds.co.uk>
2012-05-29 20:27:34 +0100

    Install Jenkins

--- puppet/modules/aegir_ci_jenkins/manifests/init.pp
+++ puppet/modules/aegir_ci_jenkins/manifests/init.pp
@@ -1,11 +1,35 @@
-class aegir_ci_jekins() {
+class aegir_ci_jenkins() {
+
+  file {'/etc/apt/sources.list.d/jenkins.list':
+    owner => 'root',
+    group => 'root',
+    mode => 644,
+    ensure => 'present',
+    content => "deb http://pkg.jenkins-ci.org/debian binary/"
+  }
+
+  exec { "jenkins apt key":
+		command => "/usr/bin/wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | /usr/bin/apt-key add -",
+		subscribe => File['/etc/apt/sources.list.d/jenkins.list'],
+		refreshonly => true,
+	}
+
+  exec { "jenkins apt update":
+		command => "/usr/bin/apt-get update",
+		subscribe => File['/etc/apt/sources.list.d/jenkins.list'],
+		refreshonly => true,
+	}
 
   package {'jenkins':
     ensure => 'latest',
+    require => [File['/etc/apt/sources.list.d/jenkins.list'], Exec['jenkins apt key']],
   }
 
   service {'jenkins':
     ensure => 'running',
-    'requires' => Package['jenkins'],
+    require => Package['jenkins'],
   }
+
+
+
 }
