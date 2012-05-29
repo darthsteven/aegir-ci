aaeaf53fcfe185f1d016d8c9fd875ef859ace3f2
Steven Jones <steven.jones@computerminds.co.uk>
2012-05-29 19:29:10 +0100

    Strip back the existing setup

--- puppet/modules/aegir_ci_server/manifests/init.pp
+++ puppet/modules/aegir_ci_server/manifests/init.pp
@@ -1,4 +1,4 @@
-class pergola_server() {
+class aegir_ci_server() {
 
   # Ensure ntp is installed.
   class { ntp:
@@ -7,38 +7,6 @@ class pergola_server() {
   }
 
   # Ensure the auto updater is around
-  class { pergola_updater:
-
+  class { aegir_ci_updater:
   }
-
-  # @TODO: these should really get moved somewhere better.
-  file {"/etc/facts.d":
-    ensure => 'directory',
-  }
-  file {"/etc/pergola":
-    ensure => 'directory',
-  }
-
-  # Ensure Varnish is installed
-  pergola_varnish { 'drupal':}
-
-  # MySQL
-  class { pergola_mysql: }
-
-  # Apache
-  class { pergola_apache: }
-
-  class { pergola_php: }
-
-  class { pergola_drush: }
-
-  class { pergola_solr: }
-
-  class { pergola_security: }
-
-  class { pergola_postfix: }
-
-  class { pergola_motd: }
-  
-  class { pergola_memcache: }
 }
